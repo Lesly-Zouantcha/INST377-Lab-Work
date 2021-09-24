d1e2d69881afcf82bfd9569063fac31da3802866
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-09-23 20:33:15 -0400

    mades lots of changes

---
+++ client/lab_4/script.js
@@ -0,0 +1,41 @@
+
+let slidePosition = 0
+const slides = document.querySelectorAll('carousel_item')
+const totalSlides = slides.length;
+
+document.querySelector('next_button')
+.addEventListener("click", function()){
+    moveToNextSlide())
+}
+
+document.querySelector('previous_button')
+.addEventListener("click", function()){
+    moveToPrevSlide())
+}
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible' )
+}
+
+function moveToNextSlide() {
+    if (slidePosition === totalSlides - 1){
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function moveToPrevSlide() {
+    if (slidePosition === 0){
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
