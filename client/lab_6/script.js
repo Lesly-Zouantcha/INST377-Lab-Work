44332eb41e8c95095561ec3f49c60c183fd88de1
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-10-07 23:08:15 -0400

    changes

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,41 +1,135 @@
-async function windowActions() {
-  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const searchInput = document.querySelector('.search');
-  const suggestions = document.querySelector('.suggestions');
 
-  const request = await fetch(endpoint);
 
-  const cities = await request.json();
+async function setup() {
+  /**
+     * API Endpoint
+     *
+     * @type {string}
+     */
+  const endpoint = "https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json";
 
-  function findMatches(wordToMatch, cities) {
-    return cities.filter((place) => {
-      const regex = new RegExp(wordToMatch, 'gi');
+  /**
+     * API Search Request
+     *
+     * @type {Object}
+     */
+  let request = await fetch(endpoint);
 
-      return place.city.match(regex);
+  /**
+     * API Search Result
+     *
+     * @type {Object}
+     */
+  let data = await request.json();
+
+  /**
+     * Query Result Table
+     *
+     * @type {DOMElement}
+     */
+  const table = document.querySelector("#result-table");
+
+  /**
+     * Query Result Table tbody
+     *
+     * @type {DOMElement}
+     */
+  const tableResults = document.querySelector("#result-table-results");
+
+  /**
+     * Query No Results Notice
+     *
+     * @type {DOMElement}
+     */
+  const noResults = document.querySelector("#no-results");
+
+  /**
+     * Search Query Form
+     *
+     * @type {DOMElement}
+     */
+  const searchForm = document.querySelector("#search-form");
+
+  /**
+     * Search Query Term Input
+     *
+     * @type {DOMElement}
+     */
+  const searchTerm = document.querySelector("#search-term");
+
+  /**
+     * Find search result matches by term
+     *
+     * @param {Object} Firing event
+     * @param {Object} Search Data
+     */
+
+  function findMatches(e, data = []) {
+    // Validate input
+    if (searchTerm.value.length <= 2) {
+      buildResultUI();
+      return;
+    }
+
+    // Variables
+    const query = searchTerm.value.toLowerCase(); // Case insensitive
+    const basis = document.querySelector('input[name="search_type"]:checked').value;
+    let results = [];
+
+
+    // Build UI with results
+    buildResultUI(results);
+  }
+
+  /**
+     * Build the result UI section
+     *
+     * @param {Array} [results=[]]
+     */
+    
+  function buildResultUI(results = []) {
+    // Validate input
+    if (!results || !(results instanceof Array) || results.length <= 0) {
+      noResults.classList.remove("is-hidden");
+      table.classList.add("is-hidden");
+    } else {
+      noResults.classList.add("is-hidden");
+      table.classList.remove("is-hidden");
+    }
+
+    // Variables
+    const term = searchTerm.value;
+    const regex = new RegExp(term, "gi");
+    const fragment = document.createDocumentFragment();
+
+    // Build Result Rows
+    // Limit to 26 results (Lag)
+    // We really should use pagination here
+    (results || []).splice(0, 25).forEach((resturant) => {
+      // Variables
+      const tr = document.createElement('tr');
+
+      // Attributes
+      tr.innerHTML = `<td>${resturant.name.toUpperCase()}</td><td>${resturant.city}</td><td>${resturant.state}</td><td>${resturant.zip}</td><td>${resturant.type}</td>`
+        .replace(regex, "<b class='has-background-info'>" + term.toUpperCase() + "</b>");
+
+      // Append
+      fragment.appendChild(tr);
     });
+
+    // Append
+    tableResults.innerHTML = "";
+    tableResults.appendChild(fragment);
   }
 
-  function displayMatches() {
-    const matchArray = findMatches(events.target.value, cities);
-
-    const suggestionsListHtml = matchArray.map((place) => {
-      const regex = new RegExp(events.target.value, 'gi');
-      const cityName = place.city.replace(regex, `<span class="hl">${events.target.value}</span>`);
-      const stateName = place.state.replace(regex, `<span class="hl">${events.target.value}</span>`);
-      const placeName = place.name.replace(regex, `<span class="hl">${events.target.value}</span>`);
-
-      return `
-                <li>
-                    <span class="name">${placeName},${cityName}, ${stateName}</span>
-                </li>
-            `;
-    }).join('');
-
-    suggestions.innerHTML = suggestionsListHtml;
-  }
-
-  searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+  // Event Listeners
+  searchForm.onsubmit = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    findMatches(e, data);
+  };
+  searchTerm.onkeyup = (e) => findMatches(e, data);
 }
 
-window.onload = windowActions();
+// Load API data automatically
+window.onload = (e) => setup();
